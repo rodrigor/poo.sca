d6cee416413c9b492284ee43782ff3ad088486ab
Rodrigo Rebouças <rodrigor@dce.ufpb.br>
2012-12-11 15:26:01 -0300

    Comentários

--- src/poo/sca/ui/SCA.java
+++ src/poo/sca/ui/SCA.java
@@ -1,3 +1,9 @@
+/**
+ * Universidade Federal da Paraíba - Campus IV
+ * Cursos: Sistemas de Informação / Lic. em Ciência da Computação
+ * Prof.: Rodrigo Rebouças de Almeida (http://rodrigor.com)
+ */
+
 package poo.sca.ui;
 
 import java.util.Iterator;
